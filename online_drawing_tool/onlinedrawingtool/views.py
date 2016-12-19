29d4b146e9642b7a773d9500e35c8203b6a5be1c
phat1402 <dtp.dangtoanphat@gmail.com>
2016-12-19 12:55:24 +0700

    UPDATE NEWSFEED

--- online_drawing_tool/onlinedrawingtool/views.py
+++ online_drawing_tool/onlinedrawingtool/views.py
@@ -8,6 +8,7 @@ from django.views import generic
 from django.views.generic.base import ContextMixin
 from api.models import UserInfor, Photolike, Photo, Followed, Comment
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
 
 
 class HomePage(generic.TemplateView):
@@ -27,6 +28,16 @@ class MyGallery(generic.TemplateView):
         kwargs['username'] = request.session.get('username')
         kwargs['numberofphoto'] = number_of_photos
         return super(MyGallery, self).get(request, *args, **kwargs)
+
+class FriendGallery(generic.TemplateView):
+    template_name = 'friend_gallery.html'
+
+    def get(self, request, *args, **kwargs):
+        photos = Photo.objects.filter(username=kwargs['username'])
+        number_of_photos = photos.count()
+        kwargs['photos'] = photos
+        kwargs['numberofphoto'] = number_of_photos
+        return super(FriendGallery, self).get(request, *args, **kwargs)
 
 class ColoringPage(generic.TemplateView):
     template_name = 'coloringpage.html'
@@ -114,15 +125,25 @@ class NewsFeed(generic.TemplateView):
 
     def get(self, request, *args, **kwargs):
         username = request.session.get('username')
-        followeds = Followed.objects.filter(username= username)
+        followeds = Followed.objects.filter(username=username)
         photo_list = []
+        photo_list_explore = []
+        username_array =[]
         for user_follow in followeds:
             username_follow = user_follow.followed
-            photos = Photo.objects.filter(username=username_follow )
+            photos = Photo.objects.filter(username=username_follow)
             for photo in photos:
                 photo_list.append(photo)
-
+            username_array.append(username_follow)
+        username_array.append(username)
+        user_not_follow = UserInfor.objects.exclude(username__in = username_array)
+        for user in user_not_follow:
+            username_not_follow = user.username
+            photos = Photo.objects.filter(username=username_not_follow)
+            for photo in photos:
+                photo_list_explore.append(photo)
         kwargs['photo_list'] = photo_list
+        kwargs['photo_list_explore'] = photo_list_explore
         return super(NewsFeed, self).get(request, *args, **kwargs)
 
 class FriendImageDetail(generic.TemplateView):
